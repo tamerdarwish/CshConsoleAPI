400d7f5b82f7bb4f2b1f14d12206965f394bf09e
tamerdarwish <darwishtamer99@gmail.com>
2021-06-01 20:30:53 +0300

    stam

--- ConsoleApplication/AppCommands.cs
+++ ConsoleApplication/AppCommands.cs
@@ -15,6 +15,10 @@ namespace CshConsoleAPI
 	{
 		public const string CMD_PROMPED = "\n>";
 		public const string CMD_ECHO = "echo";
+		public const string CMD_ADD = "add";
+		public const string CMD_SUB = "sub";
+		public const string CMD_MULT = "mult";
+		public const string CMD_DIV = "div";
 
 
 
@@ -29,6 +33,62 @@ namespace CshConsoleAPI
 			foreach (string param in parameters){
 				Console.WriteLine(param);
 			}
+			return (true);
+		}
+		public static bool CommandAdd(string[] parameters)
+		{
+			int sum = 0;
+			foreach (string param in parameters)
+			{
+				sum += Convert.ToInt32(param);
+
+			}
+			Console.WriteLine(sum);
+			return (true);
+		}
+		public static bool CommandSub(string[] parameters)
+		{
+			int sub1 = 0;
+			foreach (string param in parameters)
+			{
+				sub1 = Convert.ToInt32(param);
+				sub1--;
+
+			}
+			Console.WriteLine(sub1);
+			return (true);
+		}
+		public static bool CommandMult(string[] parameters)
+		{
+			int mult = 1;
+			foreach (string param in parameters)
+			{
+				mult *= Convert.ToInt32(param);
+
+			}
+			Console.WriteLine(mult);
+			return (true);
+		}
+		public static bool CommandDiv(string[] parameters)
+		{
+			double div = 0;
+			bool first = true;
+			foreach (string param in parameters)
+			{
+				if (first)
+				{
+					first = false;
+					div = Convert.ToDouble(param);
+
+				}
+				else
+				{
+					div /= Convert.ToDouble(param);
+				}
+
+
+			}
+			Console.WriteLine(div);
 			return (true);
 		}
 	}
--- ConsoleApplication/ConsoleApplication.cs
+++ ConsoleApplication/ConsoleApplication.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Numerics;
+//using System.Numerics;
 
 namespace CshConsoleAPI
 {
@@ -22,12 +22,15 @@ namespace CshConsoleAPI
 
          // Assign the echo command function to the command list.
          CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ECHO, AppCommands.CommandEcho);
+            CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ADD, AppCommands.CommandAdd);
+            CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_SUB, AppCommands.CommandSub);
+            CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_MULT, AppCommands.CommandMult);
+            CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_DIV, AppCommands.CommandDiv);
+            // Store user input text.
+            string command_line;
 
-         // Store user input text.
-         string command_line;
-
-         // Continues loop.
-         while (running)
+            // Continues loop.
+            while (running)
          {
             // Print command promped '>'
             Console.Write(AppCommands.CMD_PROMPED);
